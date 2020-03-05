27d518a241f77841303f91a0f6c88367bba1f0dc
Elvis Ning <ningelvis@gmail.com>
2020-03-05 20:52:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,7 @@ def monopoly_with_third_tier
 	    :railroads => {
 	      :pieces => 4,
 	      :rent_in_dollars => {
+<<<<<<< HEAD
 	        :one_piece_owned=>25,
 	        :two_pieces_owned=>50,
 	        :three_pieces_owned=>100,
@@ -37,6 +38,10 @@ def monopoly_with_third_tier
 	        :pennsylvania_railroad=>{},
 	        :b_and_o_railroad=>{},
 	        :shortline_railroad=>{}
+=======
+	        
+	      }
+>>>>>>> 0171552af39e397ed375e8a1f92a03aa59a57f50
 	      }
 	    }
   }
